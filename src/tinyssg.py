e0dfab1877314668f2d62a759d20a51b41186cdc
Ivan Movchan <ivan.movchan.07@gmail.com>
2025-08-01 23:45:37 +0300

    Implement loading specific variables for files specified in configuration
    Signed-off-by: Ivan Movchan <ivan.movchan.07@gmail.com>

--- src/tinyssg.py
+++ src/tinyssg.py
@@ -79,6 +79,15 @@ for source_dir in directories:
         
         page_text = template_text.replace('{content}', page_content).replace('{title}', source_file_content[0]).replace('{datetime}', page_datetime)
         
+        file_dir = file[2:file.rfind('/')] + '/'
+        if file_dir in spec_variables:
+            for var in spec_variables[file_dir]:
+                page_text = page_text.replace(('{' + var + '}'), spec_variables[file_dir][var])
+        
+        if file in spec_variables:
+            for var in spec_variables[file]:
+                page_text = page_text.replace(('{' + var + '}'), spec_variables[file][var])
+        
         for var in text_variables:
             page_text = page_text.replace(('{' + var + '}'), text_variables[var])
         
