62de0b3a6c9e41d357cff164dc625c162045462c
Ivan Movchan <ivan.movchan.07@gmail.com>
2026-01-24 17:59:44 +0300

    Move __init__.py code to xsitegen.py module (fixes PyInstaller build errors)
    Signed-off-by: Ivan Movchan <ivan.movchan.07@gmail.com>

--- xsitegen.py
+++ xsitegen.py
@@ -2,6 +2,43 @@
 # This file is a part of XSiteGen project.
 # See LICENSE for copyright and licensing details.
 
+VERSION = '1.4-develop'
+PYTHON_VERSION_MINOR_REQUIRED = 10
+
+import os
+import sys
+
+from xsitegen.utils import die
+
+def version():
+    die(f'XSiteGen {VERSION}\nCopyright (с) 2025-2026 Ivan Movchan\nhttps://github.com/ivan-movchan/xsitegen')
+
+def usage():
+    die(f'Usage: xsitegen [-hv]')
+
+def check_arguments():
+    if len(sys.argv) > 1:
+        for arg in sys.argv[1:]:
+            if arg == '-v':
+                version()
+            elif arg == '-h':
+                usage()
+            else:
+                print(f'Unknown argument: {arg}.')
+                usage()
+
+def check_python_version():
+    if sys.version_info.minor < PYTHON_VERSION_MINOR_REQUIRED:
+        die(f'Python 3.{PYTHON_VERSION_MINOR_REQUIRED} or newer is required.', 3)
+    else:
+        print(f'Powered by Python {'.'.join(map(str, sys.version_info))}.')
+
+if __name__ != '__main__':
+    die('This module was incorrectly imported. Terminating.', 42)
+
+check_arguments()
+check_python_version()
+
 from xsitegen.core import main
 
 main()
