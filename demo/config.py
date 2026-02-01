63c6a762850cf7f1cafad3b0427ef9c1c57a0072
Ivan Movchan <ivan.movchan.07@gmail.com>
2026-02-01 16:23:31 +0300

    Update demo site files
    Signed-off-by: Ivan Movchan <ivan.movchan.07@gmail.com>

--- demo/config.py
+++ demo/config.py
@@ -1,21 +1,27 @@
-directories = { './src/files': './build' }
-template_files = { './src/files': './src/templates/template.html' }
+directories = { 'src/files': './build' }
+template_files = { 'src/files': 'src/templates/template.html' }
 
 global_variables = {
     'menu': 'Home &bull; <a href="me/index.html">About me</a>',
     'name': '{author}\'s Blog',
     'copyright': 'Copyright &copy; 2025-2026 {author}.',
     'author': 'John Doe',
-    'powered_by': '<a href="https://github.com/ivan-movchan/xsitegen" target="_blank">Powered by XSiteGen</a>'
+    'stylesheet': 'assets/css/style.css'
 }
 
 directory_variables = {
-    './src/files/blog': {
-        'menu': '<a href="../index.html">Home</a> &bull; <a href="../me/index.html">About me</a>'
+    'src/files/blog': {
+        'menu': '<a href="../index.html">Home</a> &bull; <a href="../me/index.html">About me</a>',
+        'stylesheet': '../assets/css/style.css'
     },
-    './src/files/me': {
-        'menu': '<a href="../index.html">Home</a> &bull; About me'
+    'src/files/me': {
+        'menu': '<a href="../index.html">Home</a> &bull; About me',
+        'stylesheet': '../assets/css/style.css'
     }
 }
 
-variable_priority_list = [ 'global', 'directory', 'page' ]
+variable_priority_list = [ 'page', 'global', 'directory' ]
+
+indent_size = 8
+
+overwrite_pages = overwrite_files = True
--- demo/src/files/index.md
+++ demo/src/files/index.md
@@ -2,14 +2,8 @@
 title: {name}
 ---
 
+<img style="float: right;" src="assets/images/picture.png">
+
 # {name}
 
-<img style="float:right;" src="picture.png">
-
-Hello! This is an example of a static website (blog) that can be built with XSiteGen.
-
-### 2025
-
-- June 16: [Article 1](blog/article1.html)
-- June 16: [Article 2](blog/article2.html)
-- June 16: [Article 3](blog/article3.html)
+#### [Hello, world!](blog/hello-world.html) *(2026-02-01)*
